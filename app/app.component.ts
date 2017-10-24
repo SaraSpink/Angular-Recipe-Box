312ef3e32998cc7652e8ceff894b29b3c0384e31
Gabi Gavric and Sara Spink <gabi@>
2017-10-24 10:42:34 -0700

    lists ingredients and directions in a nested li tag inside recipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,25 +6,28 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Today's Date: {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-       <li (click)="isFavorite(recipeType)" *ngFor="let recipeType of recipes">{{recipeType.title}} <br> {{recipeType.directions}} <br> {{recipeType.ingredients}} <br>
-       git \
-       <button (click)="editRecipe()">Edit!</button></li>
-    </ul>
+    <ol>
+       <li (click)="isFavorite(recipeType)" *ngFor="let recipeType of recipes">{{recipeType.title}}  {{recipeType.directions}}
+            <ul>
+              <li>{{recipeType.ingredients}}</li>
+            </ul>
+        <button (click)="editRecipe()">Edit!</button>
+       </li>
+    </ol>
   </div>
   `
 })
 
 export class AppComponent {
-  currentFocus: string = 'Recipe Box';
+  currentFocus: string = 'Recipe Box!!!';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   recipes: Recipe[] = [
-    new Recipe('Chocolate Chip Cookies', 'Ingredients', 'Directions'),
-    new Recipe('Chocolate Chip Cookies', 'Ingredients', 'Directions'),
-    new Recipe('Chocolate Chip Cookies', 'Ingredients', 'Directions')
+    new Recipe('Chocolate Chip Cookies', ['chocolate', 'sugar', 'butter'], 'bake it'),
+    new Recipe('Pizza', ['dough', 'sauce', 'veggies'], 'toss it'),
+    new Recipe('Broth', ['bones', 'water', 'spices'], 'boil it for a long time!')
   ];
 
   editRecipe() {
@@ -40,5 +43,5 @@ export class AppComponent {
 
 export class Recipe {
   public favorite: boolean = false;
-  constructor(public title: string, public ingredients: string, public directions: string) { }
+  constructor(public title: string, public ingredients: string[], public directions: string) { }
 }
